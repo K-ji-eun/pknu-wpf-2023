999f4e28d7cfbcc3a9b4840832a481b9d66af87f
K-ji-eun <snstkfkawldm@naver.com>
2023-05-01 17:02:18 +0900

    5월 1일 월요일

--- Day10/Day10WpfApp/wp13_Festival/MainWindow.xaml.cs
+++ Day10/Day10WpfApp/wp13_Festival/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MahApps.Metro.Controls;
+using MySqlConnector;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -31,24 +33,6 @@ namespace wp13_Festival
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private async void BtnFestival_Click(object sender, RoutedEventArgs e)
-        {
-
-            if (string.IsNullOrEmpty(TxtFestivalName.Text))
-            {
-                await Commons.ShowMessageAsync("검색", " 검색할 축제 정보를 입력하세요.");
-                return;
-            }
-
-
-            if (TxtFestivalName.Text.Length <= 2)
-            {
-                await Commons.ShowMessageAsync("검색", "검색어를 2자이상 입력하세요.");
-                return;
-            }
-
         }
 
         private async void BtnReqtime_Click(object sender, RoutedEventArgs e)
@@ -112,14 +96,127 @@ namespace wp13_Festival
 
         }
 
-        private void BtnSaveData_Click(object sender, RoutedEventArgs e)
+        // 검색한 결과 DB(MySQL)에 저장
+        private async void BtnSaveData_Click(object sender, RoutedEventArgs e)
         {
+            if (GrdResult.Items.Count == 0)
+            {
+                await Commons.ShowMessageAsync("오류", "조회하고 저장하세요.");
+                return; 
+            }
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Commons.myConnString))
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                    var query = @"INSERT INTO festivalin
+                                            (
+                                            UC_SEQ,
+                                            MAIN_TITLE,
+                                            GUGUN_NM,
+                                            MAIN_PLACET,
+                                            CNTCT_TEL,
+                                            HOMEPAGE_URL)
+                                            VALUES
+                                            (
+                                            @UC_SEQ,
+                                            @MAIN_TITLE,
+                                            @GUGUN_NM,
+                                            @MAIN_PLACET,
+                                            @CNTCT_TEL,
+                                            @HOMEPAGE_URL)";
 
+                    var insRes = 0;
+                    foreach (var temp in GrdResult.Items)
+                    {
+                        if (temp is FestivalInfo)
+                        {
+                            var item = temp as FestivalInfo;
+
+                            MySqlCommand cmd = new MySqlCommand(query, conn);
+                            cmd.Parameters.AddWithValue("@UC_SEQ", item.UC_SEQ);
+                            cmd.Parameters.AddWithValue("@MAIN_TITLE", item.MAIN_TITLE);
+                            cmd.Parameters.AddWithValue("@GUGUN_NM", item.GUGUN_NM);
+                            cmd.Parameters.AddWithValue("@MAIN_PLACET", item.MAIN_PLACET);
+                            cmd.Parameters.AddWithValue("@CNTCT_TEL", item.CNTCT_TEL);
+                            cmd.Parameters.AddWithValue("@HOMEPAGE_URL", item.HOMEPAGE_URL);
+
+                            insRes += cmd.ExecuteNonQuery();
+                        }
+                    }
+                    await Commons.ShowMessageAsync("저장", "DB 저장 성공");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Commons.ShowMessageAsync("오류", $"DB저장 오류!{ex.Message}");
+            }
         }
+
 
         private void TxtFestivalName_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Enter)
+            {
+                BtnSearchFestival_Click(sender, e);
+            }
+        }
 
+        private async void BtnSearchFestival_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtFestivalName.Text))
+            {
+                await Commons.ShowMessageAsync("검색", " 검색할 축제 정보를 입력하세요.");
+                return;
+            }
+
+            this.DataContext = null;
+            //TxtFestivalName.Text = string.Empty;
+            //string txtsearch = TxtFestivalName.Text;
+
+            List<FestivalInfo> list  = new List<FestivalInfo>();
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Commons.myConnString))
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                    var query = $@"SELECT Id
+                                       , UC_SEQ
+                                       , MAIN_TITLE
+                                       , GUGUN_NM
+                                       , MAIN_PLACET
+                                       , CNTCT_TEL
+                                       , HOMEPAGE_URL
+                                    FROM festivalin
+                                    WHERE MAIN_TITLE LIKE '%{TxtFestivalName.Text}%'
+                                    ORDER BY Id ASC";
+
+                    var cmd = new MySqlCommand(query, conn);
+                    var adapter = new MySqlDataAdapter(cmd);
+                    var dSet = new DataSet();
+                    adapter.Fill(dSet, "festivalin");
+
+                    foreach (DataRow dr in dSet.Tables["festivalin"].Rows)
+                    {
+                        list.Add(new FestivalInfo
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            UC_SEQ = Convert.ToInt32(dr["UC_SEQ"]),
+                            MAIN_TITLE = Convert.ToString(dr["MAIN_TITLE"]),
+                            GUGUN_NM = Convert.ToString(dr["GUGUN_NM"]),
+                            MAIN_PLACET = Convert.ToString(dr["MAIN_PLACET"]),
+                            CNTCT_TEL = Convert.ToString(dr["CNTCT_TEL"]),
+                            HOMEPAGE_URL = Convert.ToString(dr["HOMEPAGE_URL"])
+                        });
+                    }
+
+                    this.DataContext = list;
+                }
+            }
+            catch(Exception ex)
+            {
+                await Commons.ShowMessageAsync("오류", $"DB검색 오류 {ex.Message}");
+            }
         }
     }
 }
